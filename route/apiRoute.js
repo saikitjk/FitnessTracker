26b9050108d7cf38ea42ee55083fa918fc055da8
JK <saikitjk@gmail.com>
2020-10-24 23:12:17 -0700

    created apiRoute.js and added get all workouts api call

---
+++ route/apiRoute.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.router();
+const db = require("../model");
+const Workout = require("../models/workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find()
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
+
+/*
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({ body }, res) => {
+  Transaction.create(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/transaction/bulk", ({ body }, res) => {
+  Transaction.insertMany(body)
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.get("/api/transaction", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbTransaction => {
+      res.json(dbTransaction);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
+
+*/
